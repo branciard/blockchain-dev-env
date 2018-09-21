c3a09316946e520ad4a6f54ec0f1e054cb77594f
fbranciard <francois.branciard@iex.ec>
2018-09-21 15:04:04 +0200

    Update bootstrap-ubuntu-aws.sh

--- bootstrap-ubuntu-aws.sh
+++ bootstrap-ubuntu-aws.sh
@@ -21,6 +21,9 @@ apt-get update
 apt-get install -y jq
 apt-get install -y bc
 
+# need for parity-bridge compile
+apt-get install -y pkgconfig
+
 #install build essential
 apt-get install -y build-essential openssl libssl-dev libudev-dev
 
