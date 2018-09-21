05412bb824809a24a336ff3589ec043b408d39e4
fbranciard <francois.branciard@iex.ec>
2018-09-21 15:05:36 +0200

    Update bootstrap-ubuntu-vagrant.sh

--- bootstrap-ubuntu-vagrant.sh
+++ bootstrap-ubuntu-vagrant.sh
@@ -21,7 +21,7 @@ apt-get install -y jq
 apt-get install -y bc
 
 # need for parity-bridge compile
-apt-get install -y pkgconfig
+apt-get install -y pkg-config
 
 #install build essential
 apt-get install -y build-essential openssl libssl-dev libudev-dev
